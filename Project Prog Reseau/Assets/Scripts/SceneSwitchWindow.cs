246cf7d3a0a25a0f8bbcf4ebd5948555ac18ec79
Enki <enkilonde@gmail.com>
2016-10-13 17:37:36 +0200

    ajout de la scene Tank

---
+++ Project Prog Reseau/Assets/Scripts/SceneSwitchWindow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections;
+using System;
+#if UNITY_EDITOR
+public class SceneSwitchWindow : UnityEditor.EditorWindow
+{
+
+
+	[UnityEditor.MenuItem("Tools/SceneSwitcher")]
+	internal static void CreateWindow()
+	{
+		SceneSwitchWindow Window = (SceneSwitchWindow)GetWindow(typeof(SceneSwitchWindow), false, "SceneSwitch");
+	}
+
+
+	internal void OnGUI()
+	{
+		//GUI de la fenetre ici
+		UnityEditor.EditorGUILayout.BeginVertical();
+
+		GUILayout.Label("Scenes In Build", UnityEditor.EditorStyles.boldLabel);
+		for (var i = 0; i < UnityEditor.EditorBuildSettings.scenes.Length; i++)
+		{
+			var scene = UnityEditor.EditorBuildSettings.scenes[i];
+			var sceneName = Path.GetFileNameWithoutExtension(scene.path);
+			var pressed = GUILayout.Button(i + ": " + sceneName, new GUIStyle(GUI.skin.GetStyle("Button")) { alignment = TextAnchor.MiddleLeft });
+
+			if (pressed)
+			{
+
+				if (UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+				{
+					UnityEditor.SceneManagement.EditorSceneManager.OpenScene(scene.path);
+				}
+
+			}
+		}
+		UnityEditor.EditorGUILayout.EndVertical();
+	}
+
+}
+#endif
