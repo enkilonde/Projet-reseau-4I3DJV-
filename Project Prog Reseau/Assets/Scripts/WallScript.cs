30481a2edd300b8fad1c76aaa7cab96c1723aab0
Arckantox <arckantox77@hotmail.fr>
2016-11-30 15:21:10 +0100

    Rebond des balles sur les mur avec préfab de mur

---
+++ Project Prog Reseau/Assets/Scripts/WallScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class WallScript : NetworkBehaviour
+{
+
+	void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.tag == "Bullet")
+        {
+            Vector3 vel = coll.relativeVelocity;
+            Vector3 normal = coll.contacts[0].normal;
+
+            coll.gameObject.GetComponent<Rigidbody>().velocity = Vector3.Reflect(vel, normal);
+        }
+    }
+}
